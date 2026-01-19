31cc948444454b3daa2fafd25b17d383dbf0bd27
Torbjørn Cunis <torbjoern.cunis@gmail.com>
2026-01-19 09:13:14 +0100

    Minor changes of style.

--- demo/sosfeasibility.m
+++ demo/sosfeasibility.m
@@ -1,4 +1,4 @@
-% sos feasibility optimization.
+% Check sum-of-squares feasibility
 
 % indeterminate variable
 x = casos.Indeterminates('x',2);
@@ -12,11 +12,9 @@ sos = struct('g',p);
 % constraint is scalar SOS cone
 opts = struct('Kc',struct('sos',1));
 
-
-% turn off error on fail otherwise it throws an error if infeasible; we
-% want to manually check it below
+% turn off error on fail, otherwise it throws an error if infeasible
+% we want to manually check it below
 opts.error_on_fail = 0;
-
 
 % solve by relaxation to SDP
 S = casos.sossol('S','mosek',sos,opts);
