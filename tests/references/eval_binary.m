7f8c83f083cf321f08db773684e546a8f5606a97
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-03-23 18:23:11 +0100

    Evaluate binary and repmat operations on multipoly polynomials.

---
+++ tests/references/eval_binary.m
@@ -0,0 +1,45 @@
+function res = eval_binary(op,arg1,arg2)
+% Evaluate binary operation.
+
+if isrow(arg1)
+    % repeat to match rows of second argument
+    arg1 = eval_repmat(arg1,size(arg2,1),1);
+elseif isrow(arg2)
+    % repeat to match rows of first argument
+    arg2 = eval_repmat(arg2,size(arg1,1),1);
+else
+    assert(size(arg1,1) == size(arg2,1), 'Dimension mismatch.')
+end
+
+if iscolumn(arg1)
+    % repeat to match columns of second argument
+    arg1 = eval_repmat(arg1,1,size(arg2,2));
+elseif iscolumn(arg2)
+    % repeat to match columns of first argument
+    arg2 = eval_repmat(arg2,1,size(arg1,2));
+else
+    assert(size(arg1,2) == size(arg2,2), 'Dimension mismatch.')
+end
+
+switch (op)
+    case {"plus" "minus" "times"}
+        % element-wise addition, subtraction, multiplication
+        res = feval(op,arg1,arg2);
+
+    case "ldivide"
+        % left-side division B.\A
+        vars = arg1.varname;
+        arg1_double = double(subs(arg1,vars,ones(size(vars))));
+        res = times((arg1_double).\1,arg2);
+
+    case "rdivide"
+        % right-side division A./B
+        vars = arg2.varname;
+        arg2_double = double(subs(arg2,vars,ones(size(vars))));
+        res = rdivide(arg1,arg2_double);
+
+    otherwise
+        error('Not implemented: %s.', op)
+end
+
+end
