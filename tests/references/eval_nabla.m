2bede9bb0ff58e3c6d27b1d39a022d47adf6ee31
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-04-13 23:20:34 +0200

    Generate references for nabla operation on vectors and matrices.

---
+++ tests/references/eval_nabla.m
@@ -0,0 +1,28 @@
+function res = eval_nabla(mode,arg,ivar)
+% Evaluate Nabla operation.
+
+vars = arg.varname;
+
+switch (mode)
+    case "single"
+        % nabla with respect to single variable (derivative)
+        if ivar > length(vars)
+            % variable not in polynomial
+            var = {'y'};
+        else
+            var = vars(ivar);
+        end
+
+        res = jacobian(arg,var);
+
+    case "multiple"
+        % nable with respect to multiple variables (gradient / jacobian)
+        if ivar <= length(vars)
+            % replace variable
+            vars(ivar) = {'y'};
+        end
+
+        res = jacobian(arg,vars);
+end
+
+end
