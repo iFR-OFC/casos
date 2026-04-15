f99e35d169ebdaf7dd0353c33e395e408f504146
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-04-15 12:04:07 +0200

    Test poly2basis for vectors and matrices.

--- tests/core/TestPoly2Basis.m
+++ tests/core/TestPoly2Basis.m
@@ -11,6 +11,8 @@ properties (TestParameter)
     test_values  % test polynmials
     references   % reference solutions
 
+    dim1 = {1 2 3 4 5 6};
+    dim2 = {1 2 3 4 5 6};
     arg1         % index argument 1
     arg2         % index argument 2
 end
@@ -20,17 +22,85 @@ methods (TestParameterDefinition, Static)
         % Initialize test data for poly2basis operation.
         [test_values,references] = TestPoly2Basis.loadTestData("poly2basis");
 
-        arg1 = num2cell(1:size(test_values{:},2));
-        arg2 = num2cell(1:size(test_values{:},2));
+        arg1 = num2cell(1:size(test_values{:}.scalar,2));
+        arg2 = num2cell(1:size(test_values{:}.scalar,2));
     end
 end
 
-methods (Test, ParameterCombination="pairwise")
+methods (Test, ParameterCombination="pairwise", TestTags="scalar")
     function test_poly2basis(test_case, test_values, references, arg1, arg2)
         % Test poly2basis operation.
-        basis = sparsity(test_values{1,arg1});
-        actual = poly2basis(test_values{2,arg2},basis);
-        reference = references.poly2basis{arg1,arg2};
+        argument = test_values.scalar{1,arg1};
+        basis = sparsity(test_values.scalar{2,arg2});
+
+        actual = poly2basis(argument,basis);
+        reference = references.scalar.poly2basis{arg1,arg2};
+
+        % perform assertion
+        test_case.verifyClass(actual,?casadi.DM);
+        test_case.verifyEqual(full(actual),reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "column"])
+    function test_poly2basis_column(test_case, test_values, references, dim1, dim2)
+        % Test poly2basis operation on column vectors.
+        argument = test_values.vector{1,dim1};
+        basis = sparsity(test_values.vector{2,dim2});
+        
+        if ~isequal(size(argument),size(basis))
+            % size mismatch
+            test_case.verifyError(@() poly2basis(argument,basis),?MException);
+            return
+        end
+
+        % else
+        actual = poly2basis(argument,basis);
+        reference = references.column.poly2basis{dim1,dim2};
+
+        % perform assertion
+        test_case.verifyClass(actual,?casadi.DM);
+        test_case.verifyEqual(full(actual),reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "row"])
+    function test_poly2basis_row(test_case, test_values, references, dim1, dim2)
+        % Test poly2basis operation on row vectors.
+        argument = test_values.vector{1,dim1}';
+        basis = sparsity(test_values.vector{2,dim2}');
+        
+        if ~isequal(size(argument),size(basis))
+            % size mismatch
+            test_case.verifyError(@() poly2basis(argument,basis),?MException);
+            return
+        end
+
+        % else
+        actual = poly2basis(argument,basis);
+        reference = references.row.poly2basis{dim1,dim2};
+
+        % perform assertion
+        test_case.verifyClass(actual,?casadi.DM);
+        test_case.verifyEqual(full(actual),reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags="matrix")
+    function test_poly2basis_matrix(test_case, test_values, references, dim1, dim2)
+        % Test poly2basis operation on matrices.
+        argument = test_values.matrix{1,dim1};
+        basis = sparsity(test_values.matrix{2,dim2});
+        
+        if ~isequal(size(argument),size(basis))
+            % size mismatch
+            test_case.verifyError(@() poly2basis(argument,basis),?MException);
+            return
+        end
+
+        % else
+        actual = poly2basis(argument,basis);
+        reference = references.matrix.poly2basis{dim1,dim2};
 
         % perform assertion
         test_case.verifyClass(actual,?casadi.DM);
