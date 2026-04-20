ab16a52955a2e6729e6d949436fe2fe7be486d9d
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-04-20 21:38:17 +0200

    Test is_linear on vector and matrix values.

--- tests/core/TestSymbolicOperations.m
+++ tests/core/TestSymbolicOperations.m
@@ -12,18 +12,19 @@ methods (Static)
         % Load test data for symbolic operations.
         load("../references/test_values.mat","test_values_struct")
 
-        test_value = cell(2,length(test_values_struct));
+        test_value = struct;
+        
+        for type = ["scalar" "vector" "matrix"]
+            test_value.(type) = cell(size(test_values_struct.(type{:})));
 
-        for k = 1:length(test_values_struct)
-            x_cas        = casos.PS('x',test_values_struct{k}.nIndet,1);
-
-            x_monom1_cas = monomials(x_cas,test_values_struct{k}.deg);
-            x_monom2_cas = monomials(x_cas,test_values_struct{k}.deg2);
-
-            poly1_cas = casos.PD(x_monom1_cas,test_values_struct{k}.coeffs1);
-            poly2_cas = casos.PD(x_monom2_cas,test_values_struct{k}.coeffs2);
-
-            test_value(:,k) = {poly1_cas poly2_cas};
+            for k = 1:numel(test_values_struct.(type))
+                arg = test_values_struct.(type){k};
+    
+                x = casos.Indeterminates(arg.indets{:});
+                sp = casos.Sparsity.tuplet(arg.sz(1),arg.sz(2),arg.i,arg.j,x,arg.degrees);
+    
+                test_value.(type){k} = casos.PD(sp,arg.coeffs);
+            end
         end
 
         test_values = {test_value};
