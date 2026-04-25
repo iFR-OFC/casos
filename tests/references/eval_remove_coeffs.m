5dbc791cccb5d0137678be76522e1b149302cc63
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-04-25 20:23:16 +0200

    Add copyright notices.

--- tests/references/eval_remove_coeffs.m
+++ tests/references/eval_remove_coeffs.m
@@ -1,3 +1,9 @@
+% SPDX-FileCopyrightText: 2026 Institute of Flight Mechanics and Controls, University of Stuttgart
+% SPDX-FileCopyrightText: Author(s): Torbjørn Cunis <tcunis@ifr.uni-stuttgart.de>
+% SPDX-FileContributor: For a full list of contributors, see <https://github.com/ifr-ofc/casos>
+%
+% SPDX-License-Identifier: GPL-3.0-only
+
 function res = eval_remove_coeffs(arg1,arg2,p)
 % Evaluate remove_coeffs on first argument.
 % Tolerance is based on percentile of coefficients of second argument.
