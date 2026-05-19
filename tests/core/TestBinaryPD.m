e78f90057e4fe29836f6ba9b2ac9c447ead89ff7
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-05-19 11:42:15 +0200

    Test binary operations on symbolic polynomials.

---
+++ tests/core/TestBinaryPD.m
@@ -0,0 +1,196 @@
+% SPDX-FileCopyrightText: 2026 Institute of Flight Mechanics and Controls, University of Stuttgart
+% SPDX-FileCopyrightText: Author(s): Torbjørn Cunis and Jan Olucak <tcunis@ifr.uni-stuttgart.de>
+% SPDX-FileContributor: For a full list of contributors, see <https://github.com/ifr-ofc/casos>
+%
+% SPDX-License-Identifier: GPL-3.0-only
+
+classdef (TestTags="PD") TestBinaryPD < TestPolynomialOperations
+% Test binary (element-wise) operations on constant polynomials.
+
+properties (SetAccess=protected)
+    values       % test polynomials
+    references   % reference solutions
+end
+
+properties (TestParameter)
+    op = {"plus" "minus" "times" "ldivide" "rdivide"};
+
+    dim1 = num2cell(1:6);
+    dim2 = num2cell(1:6);
+    arg1 = num2cell(1:10);
+    arg2 = num2cell(1:10);
+end
+
+methods (TestClassSetup)
+    function initializeTestData(test_case)
+        % Initialize test data for binary operations.
+        test_case.loadTestData("binary");
+
+        test_case.fatalAssertLength(test_case.dim1,size(test_case.references.matrix.plus,1));
+        test_case.fatalAssertLength(test_case.dim2,size(test_case.references.matrix.plus,2));
+        test_case.fatalAssertLength(test_case.arg1,size(test_case.references.scalar.plus,1));
+        test_case.fatalAssertLength(test_case.arg2,size(test_case.references.scalar.plus,2));
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags="scalar")
+    function test_binary_scalar(test_case, op, arg1, arg2)
+        % Test binary operation.
+        value1 = test_case.values.scalar{1,arg1};
+        value2 = test_case.values.scalar{2,arg2};
+
+        reference = test_case.references.scalar.(op){arg1,arg2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                actual = feval(op,value1,value2);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                actual = ldivide(value1_deg0,value2);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                actual = rdivide(value1,value2_deg0);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "inner"])
+    function test_binary_inner(test_case, op, dim1, dim2)
+        % Test binary operation on row-by-column vectors.
+        value1 = test_case.values.vector{1,dim1}';
+        value2 = test_case.values.vector{2,dim2};
+        
+        reference = test_case.references.inner.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                actual = feval(op,value1,value2);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                actual = ldivide(value1_deg0,value2);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                actual = rdivide(value1,value2_deg0);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "outer"])
+    function test_binary_outer(test_case, op, dim1, dim2)
+        % Test binary operation on column-by-row vectors.
+        value1 = test_case.values.vector{1,dim1};
+        value2 = test_case.values.vector{2,dim2}';
+        
+        reference = test_case.references.outer.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                actual = feval(op,value1,value2);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                actual = ldivide(value1_deg0,value2);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                actual = rdivide(value1,value2_deg0);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["matrix"])
+    function test_binary_matrix(test_case, op, dim1, dim2)
+        % Test binary operation on matrices.
+        value1 = test_case.values.matrix{1,dim1};
+        value2 = test_case.values.matrix{2,dim2};
+
+        reference = test_case.references.matrix.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                test_case.evaluate_binary_matrix(op,value1,value2,reference);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary_matrix(op,value1_deg0,value2,reference);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary_matrix(op,value1,value2_deg0,reference);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+    end
+end
+
+methods
+    function evaluate_binary_matrix(test_case, op, value1, value2, reference)
+        % Evaluate binary operation on matrices.
+        if (~isrow(value1) && ~isrow(value2) && size(value1,1) ~= size(value2,1)) ...
+                    || (~iscolumn(value1) && ~iscolumn(value2) && size(value1,2) ~= size(value2,2))
+            % dimension mismatch
+            diagtext = sprintf('Dimension mismatch expected: %s vs. %s.',mat2str(size(value1)),mat2str(size(value2)));
+            test_case.verifyError(@() feval(op,value1,value2),?MException,diagtext);
+            return
+        end
+
+        % else
+        actual = feval(op,value1,value2);
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+end
+
+end
---
+++ tests/core/TestBinaryPS.m
@@ -0,0 +1,217 @@
+% SPDX-FileCopyrightText: 2026 Institute of Flight Mechanics and Controls, University of Stuttgart
+% SPDX-FileCopyrightText: Author(s): Torbjørn Cunis and Jan Olucak <tcunis@ifr.uni-stuttgart.de>
+% SPDX-FileContributor: For a full list of contributors, see <https://github.com/ifr-ofc/casos>
+%
+% SPDX-License-Identifier: GPL-3.0-only
+
+classdef (TestTags="PS") TestBinaryPS < TestSymbolicOperations
+% Test binary (element-wise) operations on constant polynomials.
+
+properties (SetAccess=protected)
+    values       % test polynomials
+    references   % reference solutions
+end
+
+properties (TestParameter)
+    op = {"plus" "minus" "times" "ldivide" "rdivide"};
+    symb1 = {false true};
+    symb2 = {false true};
+
+    dim1 = num2cell(1:6);
+    dim2 = num2cell(1:6);
+    arg1 = num2cell(1:10);
+    arg2 = num2cell(1:10);
+end
+
+methods (TestClassSetup)
+    function initializeTestData(test_case)
+        % Initialize test data for binary operations.
+        test_case.loadTestData("binary");
+
+        test_case.fatalAssertLength(test_case.dim1,size(test_case.references.matrix.plus,1));
+        test_case.fatalAssertLength(test_case.dim2,size(test_case.references.matrix.plus,2));
+        test_case.fatalAssertLength(test_case.arg1,size(test_case.references.scalar.plus,1));
+        test_case.fatalAssertLength(test_case.arg2,size(test_case.references.scalar.plus,2));
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags="scalar")
+    function test_binary_scalar(test_case, op, symb1, symb2, arg1, arg2)
+        % Test binary operation on scalars.
+        value1 = test_case.values.scalar{1,arg1};
+        value2 = test_case.values.scalar{2,arg2};
+
+        reference = test_case.references.scalar.(op){arg1,arg2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2,reference);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1_deg0,value2,reference);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2_deg0,reference);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "inner"])
+    function test_binary_inner(test_case, op, symb1, symb2, dim1, dim2)
+        % Test binary operation on row-by-column vectors.
+        value1 = test_case.values.vector{1,dim1}';
+        value2 = test_case.values.vector{2,dim2};
+        
+        reference = test_case.references.inner.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2,reference);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1_deg0,value2,reference);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2_deg0,reference);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["vector" "outer"])
+    function test_binary_outer(test_case, op, symb1, symb2, dim1, dim2)
+        % Test binary operation on column-by-row vectors.
+        value1 = test_case.values.vector{1,dim1};
+        value2 = test_case.values.vector{2,dim2}';
+        
+        reference = test_case.references.outer.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2,reference);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1_deg0,value2,reference);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary(op,symb1,symb2,value1,value2_deg0,reference);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+    end
+end
+
+methods (Test, ParameterCombination="pairwise", TestTags=["matrix"])
+    function test_binary_matrix(test_case, op, symb1, symb2, dim1, dim2)
+        % Test binary operation on matrices.
+        value1 = test_case.values.matrix{1,dim1};
+        value2 = test_case.values.matrix{2,dim2};
+
+        reference = test_case.references.matrix.(op){dim1,dim2};
+
+        switch (op)
+            case {"plus" "minus" "times"}
+                % element-wise addition, subtraction, multiplication
+                test_case.evaluate_binary_matrix(op,symb1,symb2,value1,value2,reference);
+
+            case "ldivide"
+                % left-divide by constant polynomial
+                vars = value1.indeterminates;
+                value1_deg0 = 1+subs(value1,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary_matrix(op,symb1,symb2,value1_deg0,value2,reference);
+
+            case "rdivide"
+                % right-divide by constant polynomial
+                vars = value2.indeterminates;
+                value2_deg0 = 1+subs(value2,vars,ones(length(vars),1));
+                
+                test_case.evaluate_binary_matrix(op,symb1,symb2,value1,value2_deg0,reference);
+
+            otherwise
+                test_case.assertFail(sprintf("Not implemented: %s.",op));
+        end
+    end
+end
+
+methods
+    function evaluate_binary(test_case, op, symb1, symb2, value1, value2, reference)
+        % Evaluate binary operation on non-matrices.
+        test_case.assumeTrue(symb1 || symb2, "Operands not symbolic.");
+        
+        % symbolic polynomials
+        [p1,symbol1,argument1] = test_case.get_operand(symb1,value1);
+        [p2,symbol2,argument2] = test_case.get_operand(symb2,value2);
+
+        % build symbolic function
+        expression = feval(op,p1,p2);
+        f = casos.Function('f',[symbol1 symbol2],{expression});
+
+        actual = f(argument1{:},argument2{:});
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+
+    function evaluate_binary_matrix(test_case, op, symb1, symb2, value1, value2, reference)
+        % Evaluate binary operation on matrices.
+        test_case.assumeTrue(symb1 || symb2, "Operands not symbolic.");
+        
+        % symbolic polynomials
+        [p1,symbol1,argument1] = test_case.get_operand(symb1,value1);
+        [p2,symbol2,argument2] = test_case.get_operand(symb2,value2);
+
+        if (~isrow(value1) && ~isrow(value2) && size(value1,1) ~= size(value2,1)) ...
+                    || (~iscolumn(value1) && ~iscolumn(value2) && size(value1,2) ~= size(value2,2))
+            % dimension mismatch
+            diagtext = sprintf('Dimension mismatch expected: %s vs. %s.',mat2str(size(value1)),mat2str(size(value2)));
+            test_case.verifyError(@() feval(op,p1,p2),?MException,diagtext);
+            return
+        end
+
+        % else:
+        % build symbolic function
+        expression = feval(op,p1,p2);
+        f = casos.Function('f',[symbol1 symbol2],{expression});
+
+        actual = f(argument1{:},argument2{:});
+
+        % perform assertion
+        test_case.verifyEqualPolynomial(actual,reference,"RelTol",1e-15);
+    end
+end
+
+end
