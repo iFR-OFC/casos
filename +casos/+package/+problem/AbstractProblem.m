a58e45cc9f308408fa837357ead85ed408bb372e
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2026-05-26 22:02:07 +0200

    change structure of abstract problem

--- +casos/+package/+problem/AbstractProblem.m
+++ +casos/+package/+problem/AbstractProblem.m
@@ -6,43 +6,56 @@ classdef (Abstract) AbstractProblem
 
     properties
         name % char/string
+        n    % number of decision variables
+        m    % number of constraints
         x % decision variables
         f % objective (defaults to 0)
         g % constraints (defaults to empty polynomial)
         p % optional parameters (defaults to empty polynomial)
-        Kx % struct with cone dimensions for x
-        Kc % struct with cone dimensions for g
+        Df % jacobian of cost function
+        Dg % jacobian of constraints
     end
 
     methods
-        function obj = AbstractProblem(name,x,opts)
+        function obj = AbstractProblem(nlsos)
             arguments
-                name = ""
-                x = casos.PS
-                opts.f = casos.PS(0)
-                opts.g = casos.PS
-                opts.p = casos.PS
-                opts.Kx struct = struct()
-                opts.Kc struct = struct()
+                nlsos struct = struct()
             end
 
-            obj.name = name;
-            obj.x = casos.PS(x);
-            obj.f = casos.PS(opts.f);
-            obj.g = casos.PS(opts.g);
-            obj.p = casos.PS(opts.p);
-
-            if isempty(fieldnames(opts.Kx))
-                obj.Kx = struct('lin',length(obj.x));
+            % problem size
+            if isfield(sos,'x')
+                obj.numel_x = length(sos.x);
             else
-                obj.Kx = opts.Kx;
+                obj.numel_x = 0;
+            end
+            
+            if isfield(sos,'g')
+                obj.numel_g = length(sos.g);
+            else
+                obj.numel_g = 0;
             end
 
-            if isempty(fieldnames(opts.Kc))
-                obj.Kc = struct('lin',length(obj.g));
-            else
-                obj.Kc = opts.Kc;
-            end
+            % Valid decision variable
+            % TODO
+            obj.x = nlsos.x;
+
+            % valid constraints
+            % TODO
+            obj.g = nlsos.g;
+
+            % valid cost function
+            % TODO
+            obj.f = nlsos.f;
+
+        end
+        
+        %% Getters
+        function Df = jacobian_f(obj)
+            Df = jacobian(obj.f, obj.x)
+        end
+
+        function Dg = jacobian_g(obj)
+            Dg = jacobian(obj.f, obj.x)
         end
     end
 end
