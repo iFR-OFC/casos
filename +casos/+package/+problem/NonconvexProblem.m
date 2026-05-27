cb5c2f067c49d1dd6618c6410ef2cfbc9745ca48
Fabian Geyer <fabian.geyer@ifr.uni-stuttgart.de>
2026-05-27 16:04:04 +0200

    update structure of nonconvex problem to match superclass

--- +casos/+package/+problem/NonconvexProblem.m
+++ +casos/+package/+problem/NonconvexProblem.m
@@ -1,38 +1,59 @@
 classdef NonconvexProblem < casos.package.problem.AbstractProblem
-    properties
-        Df % Jacobian of objective wrt x
-        Dg % Jacobian of constraints wrt x
-        L % symbolic Lagrangian
-        dLdx % symbolic Lagrangian gradient wrt x
+    properties (SetAccess = immutable)
+        % Core symbolic objects (in addition to AbstractProblem properties)
+        lambda_g    % multipliers for g
+    end
+    properties (SetAccess = protected)
+        % Lagrangian
+        L           % symbolic Lagrangian (SX)
+        
+        % Derivatives
+        DL          % symbolic Lagrangian gradient wrt x (SX)
+        DDL         % symbolic Lagrangian hessian wrt x (SX)
+
+        % Evaluation objects (casos.function handles)
+        Lhan
+        DLhan
     end
 
     methods
-        function obj = NonconvexProblem(name,x,opts)
+        function obj = NonconvexProblem(nlsos)
             arguments
-                name = ""
-                x = casos.PS
-                opts.f = casos.PS(0)
-                opts.g = casos.PS
-                opts.p = casos.PS
-                opts.Kx struct = struct()
-                opts.Kc struct = struct()
-                opts.Df = []
-                opts.Dg = []
-                opts.L = []
-                opts.dLdx = []
+                nlsos struct = struct()
             end
 
-            obj@casos.package.problem.AbstractProblem(name,x, ...
-                f=opts.f, ...
-                g=opts.g, ...
-                p=opts.p, ...
-                Kx=opts.Kx, ...
-                Kc=opts.Kc);
+            % Call constructor of AbstractProblem
+            obj = obj@casos.package.problem.AbstractProblem(nlsos);
 
-            obj.Df = opts.Df;
-            obj.Dg = opts.Dg;
-            obj.L = opts.L;
-            obj.dLdx = opts.dLdx;
+            % Check if problem is defined with correct dimensions etc. 
+            %TODO
+
+            % Define lagrangian multipliers
+            obj.lambda_g = casos.PS.sym('lam_g', sparsity(obj.g));
+        end
+
+        %% Getters
+        function L = get_L(obj)
+            if isempty(obj.L)
+                obj.L = obj.f + dot(obj.lambda_g, obj.g);
+            end
+            L = obj.L;
+        end
+
+        function DL = get_DL(obj)
+            % returns symbolic gradient of Lagrangian w.r.t. x
+            if isempty(obj.DL)
+                obj.DL = op2basis(jacobian(obj.get_L, obj.x));
+            end
+            DL = obj.DL;
+        end
+
+        function DDL = get_DDL(obj)
+            % returns symbolic hessian of Lagrangian w.r.t. x
+            if isempty(obj.DDL)
+                obj.DDL = op2basis(hessian(obj.get_L, obj.x));
+            end
+            DDL = obj.DDL;
         end
     end
 end
